afe5021ec91b0d099432f735fbb69e1f26f17901
Boris Mandzhukov <skipter@abv.bg>
2019-09-21 13:51:55 +0300

    Add simple puzzle kids game

---
+++ SimplePuzzleKidsGame/Assets/Scripts/Coal.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coal : MonoBehaviour
+{
+
+    [SerializeField]
+
+    private Transform coalPlace;
+
+    private Vector2 initialPosition;
+
+
+    private float deltaX, deltaY;
+    
+    public static bool locked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        deltaX = touchPos.x - transform.position.x;
+                        deltaY = touchPos.y - transform.position.y;
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    {
+                        if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                        {
+                            transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);
+                        }
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - coalPlace.position.x) <= 0.5f &&
+                        Mathf.Abs(transform.position.y - coalPlace.position.y) <= 0.5f)
+                    {
+                        transform.position = new Vector2(coalPlace.position.x, coalPlace.position.y);
+                        locked = true;
+                    }
+                    else
+                    {
+                        transform.position = new Vector2(initialPosition.x, initialPosition.y);
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ SimplePuzzleKidsGame/Assets/Scripts/GameControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject winText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Coal.locked && Chicken.locked)
+        {
+            winText.SetActive(true);
+        }
+    }
+}
